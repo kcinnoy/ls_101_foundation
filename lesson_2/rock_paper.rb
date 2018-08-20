7bddc1016fa7482a70a8115ad66b20f1370fbc05
yonnick.io <yonnick00@gmail.com>
2018-08-20 19:53:22 +0100

    update

--- lesson_2/rock_paper.rb
+++ lesson_2/rock_paper.rb
@@ -4,22 +4,22 @@ def msg_to_user(msg)
   puts "=> #{msg}"
 end
 
-def display_result (player, computer)
-  if player == 'rock' && computer == 'scissors' ||
+def display_result(player, computer)
+  if  player == 'rock' && computer == 'scissors' ||
       player == 'paper' && computer == 'rock' ||
       player == 'scissors' && computer == 'paper'
         msg_to_user "You won!"
   elsif player == computer
         msg_to_user "You draw"
   else
-        msg_to_user "You lose"  
+        msg_to_user "You lose"
   end
 end
 
 loop do
   choice = ''
 
-  #user prompt, request and validation 
+  # user prompt,request and validation
   loop do
     msg_to_user("Please choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp
@@ -33,15 +33,13 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-
   puts "You chose #{choice}; Computer chose #{computer_choice}"
 
   display_result(choice, computer_choice)
 
-  
   msg_to_user "Do you want to play again? (y/n)"
   answer = gets.chomp
-  break unless answer.downcase.starts_with?('y')
+  break unless answer.downcase.start_with?('y')
 end
 
 msg_to_user "Thanks for playing rock paper scissors!"
